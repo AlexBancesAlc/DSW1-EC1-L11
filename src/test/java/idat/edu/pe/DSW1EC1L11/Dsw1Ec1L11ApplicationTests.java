a88b639d851fe2e03842722674e37a21bb9c5a61
[AlexBAlcantara14] <[bancesalcantaralexis@gmail.com]>
2023-06-05 15:56:42 -0500

    first commit

---
+++ src/test/java/idat/edu/pe/DSW1EC1L11/Dsw1Ec1L11ApplicationTests.java
@@ -0,0 +1,13 @@
+package idat.edu.pe.DSW1EC1L11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsw1Ec1L11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
